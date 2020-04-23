64018f2b755cfb9e7cb3374917f4fa113d41f9ee
Sandra Kao <Sandra.Kao@advantech.com>
2020-04-23 14:55:08 +0800

    Renove all useless files from merge error

--- KaoSandraWeb/js/main.js
+++ KaoSandraWeb/js/main.js
@@ -1,1 +1,50 @@
-Hi vue.js
+/*{ <script>import JSON_TagPostRelationship from '/KaoSandraWeb/js/allTags/TagPostRelationship.json'</script> }*/
+var vm = new Vue({
+    el: '#container',
+    data: {
+        tagsByPostID: {
+            tagID: [],
+            tagName: '',
+            tagDestination: ''
+        },
+        tagPostRelationship: {
+            type: '',
+            name: '',
+            posts: [{
+                postID: 0,
+                postTitle: '',
+                postPreview: '',
+                postDate: '',
+                postDestination: '',
+                postViewsCount: ''
+            }],
+
+            tags: [{
+                tagID: 0,
+                tagName: ''
+            }],
+
+            postsTagsRelationship: [{
+                postID: 0,
+                tagID: 0
+            }]
+        },
+        allPosts: {},
+        allTags: {},
+        postTag: {}
+    },
+    beforeMount() {
+        apiGetTagPostJson().then(function (res) {
+            vm.tagPostRelationship = res.data;
+            vm.allPosts = res.data.posts;
+            vm.allTags = res.data.tags;
+            vm.postTag = res.data.postsTagsRelationship;
+        }).catch(function (error) {
+            console.log(error);
+        });
+    },
+    computed: {},
+    watch: {},
+    methods: {
+    }
+});
