1a95ab878dd71003daea9fd998b06817560c1889
Sandra-Kao <shuyukao0@gmail.com>
2020-06-05 10:53:22 +0800

    Do the math with posts and tags

--- js/main.js
+++ js/main.js
@@ -32,14 +32,14 @@ var vm = new Vue({
         },
         allPosts: {},
         allTags: {},
-        allPostTag: {}
+        allPostsTags: {}
     },
     beforeMount() {
         apiGetTagPostJson().then(function (res) {
             vm.tagPostRelationship = res.data;
             vm.allPosts = res.data.posts;
             vm.allTags = res.data.tags;
-            vm.allPostTag = res.data.postsTagsRelationship;
+            vm.allPostsTags = res.data.postsTagsRelationship;
         }).catch(function (error) {
             console.log(error);
         });
@@ -47,8 +47,49 @@ var vm = new Vue({
     computed: {},
     watch: {},
     methods: {
-        mergedAllPostTag: function () {
-            return $.extend(true, vm.allPosts, vm.allTags, vm.allPostTag);
+        mergedAllPostsTags: function () {
+            let mergeObject = $.extend(true, {}, this.allPosts);
+
+            for (var y in mergeObject) {
+                for (var x in this.allPostsTags) {
+                    if (mergeObject[y].postID === this.allPostsTags[x].postID) {
+                        mergeObject[y].tags.push(this.allPostsTags[x].tagID)
+                    }
+                }
+                for (var tag in mergeObject[y].tags) {
+                    for (var t in this.allTags) {
+                        if (mergeObject[y].tags[tag] === this.allTags[t].tagID) {
+                            mergeObject[y].tags.splice(tag, 1)
+                            mergeObject[y].tags.push(this.allTags[t])
+                        }
+                    }
+                }
+            }
+
+            return mergeObject;
+        },
+        groupByPosts: function () {
+            const groupedSource = _.groupBy(this.mergedAllPostsTags(), a => a.postID);
+            return groupedSource;
+        },
+        groupByTags: function () {
+            const groupedSource = _.groupBy(this.mergedAllPostsTags(), a => a.tagID);
+            return groupedSource;
+        },
+        searchTagsByPostId: function (pID) {
+            let grouped = this.mergedAllPostsTags();
+            var obj = [];
+            for (var i = 0; i < Object.keys(grouped).length; i++) {
+                if (pID == grouped[i].postID) {
+                    for (var t = 0; t < Object.keys(grouped[i].tags).length; t++) {
+                        obj.push({
+                            tagID: grouped[i].tags[t].tagID,
+                            tagname: grouped[i].tags[t].tagName
+                        })
+                    }
+                }
+            }
+            return obj;
         }
     }
 });
